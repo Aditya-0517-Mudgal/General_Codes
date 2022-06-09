1be021e1a2797b70f2ca0162a54b67ac7d840558
Aditya Mudgal <Aditya7Mudgal@gmail.com>
2022-06-09 21:18:25 +0530

    Add files via upload

---
+++ PrimeNumberOrNot.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{  int i,n;
+   clrscr();
+   printf("Enter a number : ");
+   scanf("%d",&n);
+   for(i=2;i<=n/2;i++)
+   {  if(n%i==0)
+      {  printf("This is not a prime number");
+	 break;
+      }
+      else
+      {  printf("This is a prime number");
+	 break;
+      }
+   }
+   getch();
+   return 0;
+}
